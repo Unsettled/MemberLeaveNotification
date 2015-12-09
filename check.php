991251bfb42450c1d78263383cf8a901850fe47a
Zumochi <zumpyzum@gmail.com>
2015-12-09 17:42:19 +0100

    Updated notificationID for member joining (undocumented on http://wiki.eve-id.net)

--- check.php
+++ check.php
@@ -6,8 +6,7 @@ require(WORKING_DIR . '/src/boot.php');
 // TODO: make singleton configuration class
 $notifications = array(
     new \Notification\Type(16, "MemberApplication", "applied to join", "unset-internal-affair"),
-    // This does nothing as only the player that applied receives it.
-    new \Notification\Type(18, "MemberAccepted", "application accepted", "unset-internal-affair"),
+    new \Notification\Type(128, "MemberJoined", "joined corp", "unset-internal-affair"),
     new \Notification\Type(21, "MemberLeft", "left corp", "unset-internal-affair"),
 );
 
